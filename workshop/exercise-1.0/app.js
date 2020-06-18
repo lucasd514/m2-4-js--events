feaa8a6c808fc06e4d907a3574e8c8d1056f83ab
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-18 12:53:17 -0400

    q1.0

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,20 @@
 // Exercise 1.0
 // ------------
-console.log('exercise-1');
+console.log("exercise-1");
+
+/// add variables
+
+const container = document.querySelector("body");
+const addp = document.createElement("p");
+addp.id = "sentence";
+container.style.height = "600px";
+container.appendChild(addp);
+
+const txt = document.getElementById("sentence");
+///define the fucntion
+function clickEvent() {
+  sentence.innerText = "boom goes the D";
+  container.removeEventListener("click", clickEvent);
+}
+/// add the clickevent
+container.addEventListener("mousedown", clickEvent);
