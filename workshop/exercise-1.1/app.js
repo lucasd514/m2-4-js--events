5883a1aa2b0dda5df5b533aa3b0a3a14b93f3fe3
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-18 19:27:49 -0400

    1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,34 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+console.log("exercise 1.1");
+
+const container = document.querySelector("body");
+
+const p1 = document.createElement("p");
+p1.id = "faster";
+p1.innerText = "YERRRRRRRRRRR click faster";
+container.appendChild(p1);
+const BIG = document.createElement("h1");
+let clicked = false;
+
+function onClick() {
+  console.log("yerrrrrrr");
+  BIG.innerText = "GG Homey";
+  container.appendChild(BIG);
+  clicked = true;
+}
+
+container.addEventListener("mousedown", onClick);
+
+function noClick() {
+  if (clicked === false) {
+    console.log("yerrrrrrr");
+    BIG.innerText = "GTFO WASTEMANssssz";
+    clicked = false;
+    console.log(clicked);
+    container.appendChild(BIG);
+    container.removeEventListener("mousedown", onClick);
+  }
+}
+
+setTimeout(noClick, 1000);
