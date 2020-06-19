f41110be22d7c43ac05ddbfe929ab43e149f81e1
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-18 20:51:38 -0400

    q1 done

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,30 @@
 // Exercise 1.2
 // ------------
-console.log('exercise 1.2');
+console.log("exercise 1.2");
+const clock = Math.round(Math.random() * 5);
+console.log(clock);
+const container = document.querySelector("body");
+time.innerText = clock + " seconds";
+const MSG = document.createElement("h1");
+let clicked = false;
+
+function click() {
+  MSG.innerText = "yerrrrrrrrr";
+  container.appendChild(MSG);
+  clicked = true;
+  console.log(clicked);
+}
+
+container.addEventListener("mousedown", click);
+
+function nahtho() {
+  if (clicked === false) {
+    container.appendChild(MSG);
+    clicked = false;
+    MSG.innerText = "JAJAJAJA 2 SLOW 4 da DOUGH KKKKkk";
+    console.log(clicked);
+    container.removeEventListener("mousedown", click);
+  }
+}
+
+setTimeout(nahtho, clock + "000");
